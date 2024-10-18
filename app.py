749a36469d55b588168ff2372ed06961110f647e
Rishi Raj Jain <jain71000@gmail.com>
2024-10-18 10:04:21 +0000

    ...

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from diffusers import DiffusionPipeline
 
-pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", cache_dir="./FLUX", local_files_only=True)
+pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", cache_dir="FLUX_1_dev", local_files_only=True)
 
 st.title("AI Image Generation with FLUX.1-dev")
 prompt = st.text_input("Enter your prompt:", "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k")
--- load.py
+++ load.py
@@ -2,6 +2,6 @@ import os
 from huggingface_hub import login, snapshot_download
 
 login(token=os.getenv('HF_TOKEN'))
-snapshot_download(repo_id="black-forest-labs/FLUX.1-dev", cache_dir="./FLUX")
+snapshot_download(repo_id="black-forest-labs/FLUX.1-dev", cache_dir="./FLUX_1_dev")
 
 print('Done')
