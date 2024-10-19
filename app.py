edf07fb081160d95b9166ba9d1280e56d3dee5f5
Rishi Raj Jain <jain71000@gmail.com>
2024-10-19 12:14:55 +0000

    ..

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from diffusers import DiffusionPipeline
 from huggingface_hub import login, snapshot_download
 
 login(token=os.getenv('HF_TOKEN'))
-pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", cache_dir="FLUX_1_dev", local_files_only=True)
 
 st.title("AI Image Generation with FLUX.1-dev")
 prompt = st.text_input("Enter your prompt:", "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k")
@@ -14,5 +13,6 @@ if st.button("Load Model"):
 
 if st.button("Generate Image"):
     with st.spinner("Generating image..."):
+        pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", cache_dir="FLUX_1_dev", local_files_only=True)
         image = pipe(prompt).images[0]
         st.image(image, caption=prompt)
