f504c14e261f7b770a2c185b36f016a6ff4e4be2
Rishi Raj Jain <jain71000@gmail.com>
2024-10-19 13:21:37 +0000

    ..

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from huggingface_hub import login, snapshot_download
 
 login(token=os.getenv('HF_TOKEN'))
 
-st.title("AI Image Generation with FLUX.1-dev")
+st.title("AI Image Generation with FLUX.1-schnell")
 prompt = st.text_input("Enter your prompt:", "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k")
 
 if st.button("Load Model"):
     with st.spinner("Downloading model..."):
-        snapshot_download(repo_id="black-forest-labs/FLUX.1-dev", cache_dir="./FLUX_1_dev")
+        snapshot_download(repo_id="black-forest-labs/FLUX.1-schnell", cache_dir="./FLUX_1_dev")
 
 if st.button("Generate Image"):
     with st.spinner("Generating image..."):
         try:
-            pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", cache_dir="./FLUX_1_dev")
+            pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", cache_dir="./FLUX_1_dev")
             image = pipe(prompt).images[0]
             st.image(image, caption=prompt)
         except Exception as e:
