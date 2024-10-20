93a2226ecd06d46ae26a18d7ce0e3bef71308449
Rishi Raj Jain <jain71000@gmail.com>
2024-10-20 05:58:03 +0000

    ...

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,13 @@ WORKDIR /app
 COPY app.py requirements.txt .
 
 RUN pip install -r requirements.txt --root-user-action=ignore
+RUN pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu121 --root-user-action=ignore
 
 RUN apt-get update 
 RUN apt-get install -y libsm6 libxext6 git git-lfs cuda-toolkit
 RUN rm -rf /var/lib/apt/lists/*
 
 EXPOSE 8000
-
-# RUN python3 load.py
 
 ARG HF_TOKEN
 CMD ["streamlit", "run", "./app.py", "--server.port", "8000"]
--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from huggingface_hub import login, snapshot_download
 login(token=os.getenv('HF_TOKEN'))
 
 st.title("AI Image Generation with FLUX.1-dev")
-prompt = st.text_input("Enter your prompt:", "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k")
-num_inference_steps = st.number_input("Enter number of inference steps:", min_value=1, value=3)
 
 if st.button("Load Model"):
     with st.spinner("Downloading model..."):
@@ -17,8 +15,13 @@ if st.button("Load Model"):
 if st.button("Generate Image"):
     with st.spinner("Generating image..."):
         try:
-            pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.float16, use_safetensors=True, cache_dir="./FLUX_1_dev").to("cuda")
-            images = pipe(prompt=prompt, num_inference_steps=num_inference_steps).images
+            prompt = st.text_input("Enter your prompt:", "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k")
+            num_inference_steps = st.number_input("Enter number of inference steps:", min_value=1, value=3)
+            image_ratio = st.selectbox("Select image ratio:", ["1:1", "16:9", "4:3"])
+            num_images = st.number_input("Enter number of images to generate:", min_value=1, value=1)
+            num_iterations = st.number_input("Enter number of iterations:", min_value=1, value=1)
+            pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16, cache_dir="./FLUX_1_dev")
+            images = pipe(prompt=prompt, num_inference_steps=num_inference_steps, height=image_ratio, width=image_ratio, num_images=num_images, num_iterations=num_iterations).images
             for i, image in enumerate(images):
                 st.image(image, caption=f"{prompt} - Image {i+1}")
         except Exception as e:
